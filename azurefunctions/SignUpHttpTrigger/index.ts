5338f7e17c92bb2e565aab7e457e0c3c0f144fc2
James Schuler <jameshschuler1@gmail.com>
2021-07-24 21:19:50 -0400

    initial azure function setup

---
+++ azurefunctions/SignUpHttpTrigger/index.ts
@@ -0,0 +1,52 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+interface SignUp {
+    email: string;
+    password: string;
+}
+
+function createSupabaseClient (): SupabaseClient {
+    const supabaseUrl = process.env.SUPABASE_URL!;
+    const supabaseKey = process.env.SUPABASE_KEY!;
+    return createClient( supabaseUrl, supabaseKey );
+}
+
+const httpTrigger: AzureFunction = async function ( context: Context, req: HttpRequest ): Promise<void> {
+    const supabase = createSupabaseClient();
+
+    const { email, password } = req.body as SignUp;
+
+    let { user, error } = await supabase.auth.signIn( {
+        email,
+        password
+    } );
+
+    let response;
+
+    if ( error ) {
+        response = {
+            errors: [ {
+                ...error
+            } ]
+        };
+    } else {
+        response = {
+            user: {
+                email: user?.email,
+                createdAt: user?.created_at,
+                updatedAt: user?.updated_at
+            }
+        }
+    }
+
+    console.log( user );
+
+    context.res = {
+        status: error ? 400 : 200,
+        body: response
+    };
+
+};
+
+export default httpTrigger;
